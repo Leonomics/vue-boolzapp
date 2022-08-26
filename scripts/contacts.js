f6e9230cf185494194369d250b1ed363f275164a
Leonomics <leocv97@gmail.com>
2022-08-26 14:49:56 +0200

    add header for leftContainer

---
+++ scripts/contacts.js
@@ -0,0 +1,62 @@
+const contacts = [
+    {
+        avatar:'',
+        name:'',
+        ultimo_messaggio:'',
+        date:'',
+    },
+    {
+        avatar:'',
+        name:'',
+        ultimo_messaggio:'',
+        date:'',
+    },
+    {
+        avatar:'',
+        name:'',
+        ultimo_messaggio:'',
+        date:'',
+    },
+    {
+        avatar:'',
+        name:'',
+        ultimo_messaggio:'',
+        date:'',
+    },
+    {
+        avatar:'',
+        name:'',
+        ultimo_messaggio:'',
+        date:'',
+    },
+    {
+        avatar:'',
+        name:'',
+        ultimo_messaggio:'',
+        date:'',
+    },
+    {
+        avatar:'',
+        name:'',
+        ultimo_messaggio:'',
+        date:'',
+    },
+    {
+        avatar:'',
+        name:'',
+        ultimo_messaggio:'',
+        date:'',
+    },
+]
+
+const app = new Vue({
+    el:'#app',
+    data:{
+        
+    },
+    
+    methods:{
+        
+    },
+
+})
