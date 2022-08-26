eedeb21f8474427e21fb07e7f33a05330e02b2d3
Leonomics <leocv97@gmail.com>
2022-08-26 20:01:05 +0200

    add contacts

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -1,50 +1,50 @@
 const contacts = [
     {
-        avatar:'',
-        name:'',
-        ultimo_messaggio:'',
+        avatar:'img/assets/avatar_1.jpg',
+        name:'Michele',
+        ultimo_messaggio:'Ultimo messaggio inviato',
         date:'',
     },
     {
-        avatar:'',
-        name:'',
-        ultimo_messaggio:'',
+        avatar:'img/assets/avatar_2.jpg',
+        name:'Fabio',
+        ultimo_messaggio:'Ultimo messaggio inviato',
         date:'',
     },
     {
-        avatar:'',
-        name:'',
-        ultimo_messaggio:'',
+        avatar:'img/assets/avatar_3.jpg',
+        name:'Samuele',
+        ultimo_messaggio:'Ultimo messaggio inviato',
         date:'',
     },
     {
-        avatar:'',
-        name:'',
-        ultimo_messaggio:'',
+        avatar:'img/assets/avatar_4.jpg',
+        name:'Alessandro B.',
+        ultimo_messaggio:'Ultimo messaggio inviato',
         date:'',
     },
     {
-        avatar:'',
-        name:'',
-        ultimo_messaggio:'',
+        avatar:'img/assets/avatar_5.jpg',
+        name:'Alessandro L.',
+        ultimo_messaggio:'Ultimo messaggio inviato',
         date:'',
     },
     {
-        avatar:'',
-        name:'',
-        ultimo_messaggio:'',
+        avatar:'img/assets/avatar_6.jpg',
+        name:'Claudia',
+        ultimo_messaggio:'Ultimo messaggio inviato',
         date:'',
     },
     {
-        avatar:'',
-        name:'',
-        ultimo_messaggio:'',
+        avatar:'img/assets/avatar_7.jpg',
+        name:'Federico',
+        ultimo_messaggio:'Ultimo messaggio inviato',
         date:'',
     },
     {
-        avatar:'',
-        name:'',
-        ultimo_messaggio:'',
+        avatar:'img/assets/avatar_8.jpg',
+        name:'Davide',
+        ultimo_messaggio:'Ultimo messaggio inviato',
         date:'',
     },
 ]
@@ -52,7 +52,10 @@ const contacts = [
 const app = new Vue({
     el:'#app',
     data:{
-        
+        userName:"Sofia",
+        userImage:"img/assets/avatar_io.jpg",
+        contacts: contacts,
+        classiContacts: 'contact',
     },
     
     methods:{
