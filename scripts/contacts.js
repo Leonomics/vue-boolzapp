0ed744282d0f2e8a1aa755319036708866d36a93
Leonomics <leocv97@gmail.com>
2022-08-28 11:56:02 +0200

    add the possibility to dinamically display contacts on the right side

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -3,49 +3,172 @@ const contacts = [
         avatar:'img/assets/avatar_1.jpg',
         name:'Michele',
         ultimo_messaggio:'Ultimo messaggio inviato',
-        date:'',
+        date:'12:00',
+        messages: [
+            {
+            date: '10/01/2020 15:30:55',
+            message: 'Hai portato a spasso il cane?',
+            status: 'sent'
+            },
+            {
+            date: '10/01/2020 15:50:00',
+            message: 'Ricordati di stendere i panni',
+            status: 'sent'
+            },
+            {
+            date: '10/01/2020 16:15:22',
+            message: 'Tutto fatto!',
+            status: 'received'
+            }
+        ]
     },
     {
         avatar:'img/assets/avatar_2.jpg',
         name:'Fabio',
         ultimo_messaggio:'Ultimo messaggio inviato',
-        date:'',
+        date:'12:00',
+        messages: [
+            {
+            date: '20/03/2020 16:30:00',
+            message: 'Ciao come stai?',
+            status: 'sent'
+            },
+            {
+            date: '20/03/2020 16:30:55',
+            message: 'Bene grazie! Stasera ci vediamo?',
+            status: 'received'
+            },
+            {
+            date: '20/03/2020 16:35:00',
+            message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+            status: 'sent'
+            }
+        ],
     },
     {
         avatar:'img/assets/avatar_3.jpg',
         name:'Samuele',
         ultimo_messaggio:'Ultimo messaggio inviato',
-        date:'',
+        date:'12:00',
+        messages: [
+            {
+            date: '28/03/2020 10:10:40',
+            message: 'La Marianna va in campagna',
+            status: 'received'
+            },
+            {
+            date: '28/03/2020 10:20:10',
+            message: 'Sicuro di non aver sbagliato chat?',
+            status: 'sent'
+            },
+            {
+            date: '28/03/2020 16:15:22',
+            message: 'Ah scusa!',
+            status: 'received'
+            }
+        ],
+
     },
     {
         avatar:'img/assets/avatar_4.jpg',
         name:'Alessandro B.',
         ultimo_messaggio:'Ultimo messaggio inviato',
-        date:'',
+        date:'12:00',
+        messages: [
+            {
+            date: '10/01/2020 15:30:55',
+            message: 'Lo sai che ha aperto una nuova pizzeria?',
+            status: 'sent'
+            },
+            {
+            date: '10/01/2020 15:50:00',
+            message: 'Si, ma preferirei andare al cinema',
+            status: 'received'
+            }
+        ],
     },
     {
         avatar:'img/assets/avatar_5.jpg',
         name:'Alessandro L.',
         ultimo_messaggio:'Ultimo messaggio inviato',
-        date:'',
+        date:'12:00',
+        messages: [
+            {
+            date: '10/01/2020 15:30:55',
+            message: 'Ricordati di chiamare la nonna',
+            status: 'sent'
+            },
+            {
+            date: '10/01/2020 15:50:00',
+            message: 'Va bene, stasera la sento',
+            status: 'received'
+            }
+        ],
     },
     {
         avatar:'img/assets/avatar_6.jpg',
         name:'Claudia',
         ultimo_messaggio:'Ultimo messaggio inviato',
-        date:'',
+        date:'12:00',
+        messages: [
+            {
+            date: '10/01/2020 15:30:55',
+            message: 'Ciao Claudia, hai novità?',
+            status: 'sent'
+            },
+            {
+            date: '10/01/2020 15:50:00',
+            message: 'Non ancora',
+            status: 'received'
+            },
+            {
+            date: '10/01/2020 15:51:00',
+            message: 'Nessuna nuova, buona nuova',
+            status: 'sent'
+            }
+        ]
     },
     {
         avatar:'img/assets/avatar_7.jpg',
         name:'Federico',
         ultimo_messaggio:'Ultimo messaggio inviato',
-        date:'',
+        date:'12:00',
+        
+        messages: [
+            {
+            date: '10/01/2020 15:30:55',
+            message: 'Fai gli auguri a Martina che è il suo compleanno!',
+            status: 'sent'
+            },
+            {
+            date: '10/01/2020 15:50:00',
+            message: 'Grazie per avermelo ricordato, le scrivo subito!',
+            status: 'received'
+            }
+        ],
     },
     {
         avatar:'img/assets/avatar_8.jpg',
         name:'Davide',
         ultimo_messaggio:'Ultimo messaggio inviato',
-        date:'',
+        date:'12:00',
+        messages: [
+            {
+            date: '10/01/2020 15:30:55',
+            message: 'Ciao, andiamo a mangiare la pizza stasera?',
+            status: 'received'
+            },
+            {
+            date: '10/01/2020 15:50:00',
+            message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
+            status: 'sent'
+            },
+            {
+            date: '10/01/2020 15:51:00',
+            message: 'OK!!',
+            status: 'received'
+            }
+        ],
     },
 ]
 
@@ -58,12 +181,14 @@ const app = new Vue({
         classiContacts: 'contact',
         isHovering:false,
         hoveredContact:null,
+        activeContact: null,
     },
     
     methods:{
-        selectContact(){
+        selectContact(contact){
             console.log("click")
-            
+            this.activeContact = contact.name 
+            console.log(this.activeContact) 
         },
     },
 
